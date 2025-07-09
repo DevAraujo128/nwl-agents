a491ce7df14b2b42762dbf375c9226a26c779d7d
Joao_Araujo128 <joaorodriguesjp01@gmail.com>
2025-07-09 00:31:33 -0300

    ✨ feat: adicionar biblioteca dayjs e componentes de UI para criação de salas

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,14 +1,20 @@
+
+import { Badge } from '@/components/ui/badge'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
+import { ArrowRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
 
 type GetRoomsAPIResponse = Array<{
   id: string,
-  name: string
+  name: string,
+  questionCount: number,
+  createdAt: string,
 }>
 
 export function CreateRoom() {
 
-  const { data, isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ['get-rooms'],
     queryFn: async () => {
       const response = await fetch('http://localhost:3333/rooms')
@@ -20,19 +26,52 @@ export function CreateRoom() {
 
 
   return(
-    <div>
-      <div>Create Room</div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid gap-8 grid-cols-2 items-start">
 
-      {isLoading && <p>Carregando...</p>}
+        <div />
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                Salas recentes
+                <CardDescription>
+                  Acesso rápido para as salas criadas recentemente
+                </CardDescription>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-3">
+              {data?.map(room => {
+                  return (
+                      <Link
+                      className="flex items-center justify-between rounded-lg border p-3 hover:bg-accent" 
+                      key={room.id}    
+                      to={'/rooms/${room.id}'}         
+                    >
+                      <div className="flex-1 flex flex-col gap-1">
+                        <h3 className="font-mediums">{room.name}</h3>
 
-      <div className='flex flex-col gap-1'>
-        {data?.map((room)  => {
-          return(
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          )
-        })}
+                        <div className="flex items-center gap-2">
+                          <Badge className="text-xs" variant="secondary" >
+                            {room.createdAt}
+                          </Badge>
+                          <Badge className="text-xs" variant="secondary" >
+                            {room.questionCount} Pergunta(s)
+                          </Badge>
+                        </div>
+                      </div>
+
+
+                      <span className="flex items-center gap-1 text-sm">
+                        Entrar
+                        <ArrowRight className="size-3" />
+                      </span>
+                    </Link>    
+                  )
+              })}
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   )
